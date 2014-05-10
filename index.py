0974f376f24a6ce9e6b87ba3a564f099402762b3
Qblack <blac2410@mylaurier.ca>
2014-05-10 19:36:18 -0400

    Have buttons to toggle thing on and off

--- index.py
+++ index.py
@@ -60,9 +60,11 @@ def close_db_connection(exception):
 
 
 
-@app.route('/')
+@app.route('/',methods=['POST','GET'])
 def show_all():
     db = get_db()
+    if request.method=='POST':
+        update_completes(db)
 
     entries =[]
     for homework in TYPES:
@@ -70,6 +72,7 @@ def show_all():
     homework = join_homework(entries)
 
     return render_template('show_all.html',entries=homework )
+
 
 
 
@@ -111,13 +114,12 @@ def format_datetime(value,format='%b-%d'):
 environment.DEFAULT_FILTERS['datetimeformat']=format_datetime
 
 def select_homework(homework_type,db):
-    sql = ''' SELECT * FROM {0} ORDER BY date'''.format(homework_type)
+    sql = ''' SELECT rowid,* FROM {0} ORDER BY date'''.format(homework_type)
     cur = db.execute(sql)
     entries = cur.fetchall()
     return entries
 
 def join_homework(homework):
-
     entries=[]
     i=0
     for table in homework:
@@ -128,19 +130,41 @@ def join_homework(homework):
                     name='--'.join([name,row['chapter']])
                 if None!= row['pages']:
                      name='--'.join([name,row['pages']])
-
-
             if 'time' not in row.keys():
                 time = 'in-class'
             else:
                 time = row['time']
 
-            new_row = {'type':TYPES[i],'course_code':row['course_code'],'name':name,
+            new_row = {'id':'-'.join([TYPES[i],str(row['rowid'])]),'type':TYPES[i],
+                       'course_code':row['course_code'],'name':name,
                        'date':row['date'],'time':time,'complete':row['complete']}
             entries.append(new_row)
         i+=1
     return entries
 
+def update_completes(db):
+    f = request.form
+    for key in f.keys():
+        for value in f.getlist(key):
+            table_id=key.split("-")
+            table= table_id[0]
+            rowid = table_id[1]
+            complete = int(value)
+            complete^=1
+            update_table(db,table,complete,rowid)
+    return
+
+def update_table(db,table,completed,rowid):
+    sql = '''UPDATE {0}
+            SET complete='{1}'
+            WHERE rowid = {2}
+            '''.format(table,completed,rowid)
+    db.execute(sql)
+    db.commit()
+    return
+
+
 if __name__ == '__main__':
     #init_db()
-    app.run()
+
+    app.run(host='0.0.0.0')
